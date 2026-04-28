e33d998dd4c9d970a59f917ebf7c91d7b29e966f
gajamatassa <85848212+gajamatassa@users.noreply.github.com>
2026-04-28 11:05:45 +0200

    update exploration hormone genes in ext datasets

--- 0_HormonalGenes/exploration_scRNASeq/functions.py
+++ 0_HormonalGenes/exploration_scRNASeq/functions.py
@@ -5,26 +5,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 ### MARKER GENES               
-def selectMarkers(adataObj, mList):
-    """  From a list of gene names select only the genes that are present in adata.var
-    """
-    #Select markers present in adata
-    p = adataObj.var_names[adataObj.var_names.isin(mList) == True]
-    #Keep the same order as input list
-    p = [x for x in mList if x in p]   
-    
-    #Select missing genes
-    ab = set(mList).difference(set(adataObj.var_names))
-    
-    #Print message SISTEMA
-    if len(ab) == len(mList):
-        print('\nAll markers are missing')
-    else:
-        print('\nThe following marker genes are missing: ', ab)
-        
-    return(p)
+def selectMarkers(adata, mList, var_col=None):
 
-def CustomUmap(adata, genes, embedding, s=10):
-    genes = selectMarkers(adata, genes)
-    #sc.pl.umap(adata, color=genes, size=10, frameon=False,vmin='p1',  vmax='p99')
-    sc.pl.embedding(adata,  basis=embedding, color=genes, ncols=1, size=s, frameon=False, vmin='p1',  vmax='p99')
+    p = []
+    found = set()
+
+    for g in mList:
+        if g in adata.var_names:
+            p.append(g)
+            found.add(g)
+
+        elif var_col is not None and var_col in adata.var.columns:
+            if (adata.var[var_col] == g).any():
+                p.append(g) 
+                found.add(g)
+
+    missing = set(mList) - found
+    if len(missing) == len(mList):
+        print("\nAll markers are missing")
+    elif missing:
+        print("\nMissing:", missing)
+
+    return p
+
+
+def CustomUmap(adata, genes, embedding, s=10, var_col=None, **kwargs):
+    genes = selectMarkers(adata, genes, var_col=var_col)
+    sc.pl.embedding(adata, basis=embedding, color=genes, ncols=1,
+                    size=s, frameon=False, vmin='p1', vmax='p99', **kwargs)
