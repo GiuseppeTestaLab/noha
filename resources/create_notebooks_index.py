6d66b1a28740b10f364168255007199b3f7d38f3
gajamatassa <85848212+gajamatassa@users.noreply.github.com>
2026-04-30 16:03:28 +0200

    update create_notebooks_index.py and onfig.yaml

--- resources/create_notebooks_index.py
+++ resources/create_notebooks_index.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import yaml
 import markdown
+from collections import defaultdict
 
 INDEX_TEMPLATE_HEADER = """
 <!DOCTYPE html>
@@ -10,22 +11,38 @@ INDEX_TEMPLATE_HEADER = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{1}</title>
+    <title>{short_title}</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
   <body>
     <main>
-        <h1>{0}</h1>
-        {2}
-    <p>
+      <header class="site-header">
+        <h1>{title}</h1>
+        <div class="subtitle">{subtitle}</div>
+      </header>
 """
 
-INDEX_TEMPLATE_LINKLINE = "<h2><a href={1}>{0}</a></h2>{2}"
+SECTION_TEMPLATE = """
+      <section class="notebook-section">
+        <h2 class="section-title">{section}</h2>
+        <div class="notebook-grid">
+{items}
+        </div>
+      </section>
+"""
+
+CARD_TEMPLATE = """
+          <article class="notebook-card">
+            <h3><a href="{href}">{title}</a></h3>
+            <div class="description">{description}</div>
+          </article>
+"""
 
 INDEX_TEMPLATE_FINAL = """
-      {0}
-    </p>
+      <footer class="site-footer">
+        {final}
+      </footer>
     </main>
     <script src="index.js"></script>
   </body>
@@ -33,25 +50,48 @@ INDEX_TEMPLATE_FINAL = """
 """
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("configyaml")
     args = parser.parse_args()
-    
-    with open(args.configyaml) as f:
-      config_dict = yaml.load(f, Loader=yaml.SafeLoader)
 
-    html_subtitle = markdown.markdown(config_dict["subtitle"])
-    print(INDEX_TEMPLATE_HEADER.format(config_dict["title"], config_dict["short_title"], html_subtitle))
-    
-    for k, v in config_dict["pages"].items():
-       html_description = markdown.markdown(v["description"])
-       path_to_file = v["notebook"].replace(".ipynb", ".html")  
-       file_name = os.path.basename(path_to_file)
-       print(INDEX_TEMPLATE_LINKLINE.format(k, file_name, html_description))
-       
-    html_final = markdown.markdown(config_dict["final"])
-    print(INDEX_TEMPLATE_FINAL.format(html_final))
+    with open(args.configyaml) as f:
+        config_dict = yaml.load(f, Loader=yaml.SafeLoader)
+
+    html_subtitle = markdown.markdown(config_dict.get("subtitle", ""))
+    html_final = markdown.markdown(config_dict.get("final", ""))
+
+    print(INDEX_TEMPLATE_HEADER.format(
+        title=config_dict["title"],
+        short_title=config_dict["short_title"],
+        subtitle=html_subtitle
+    ))
+
+    sections = defaultdict(list)
+
+    for key, page in config_dict["pages"].items():
+        section = page.get("section", "Other")
+        sections[section].append((key, page))
+
+    for section, items in sections.items():
+        cards = []
+
+        for key, page in items:
+            html_description = markdown.markdown(page["description"])
+            path_to_file = page["notebook"].replace(".ipynb", ".html")
+            file_name = os.path.basename(path_to_file)
+
+            cards.append(CARD_TEMPLATE.format(
+                title=key,
+                href=file_name,
+                description=html_description
+            ))
+
+        print(SECTION_TEMPLATE.format(
+            section=section,
+            items="".join(cards)
+        ))
+
+    print(INDEX_TEMPLATE_FINAL.format(final=html_final))
 
 if __name__ == '__main__':
     main()
